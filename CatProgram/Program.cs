6455886e567c789438261e48273656190ba5f2af
udwee <jonttu.92@hotmail.com>
2019-01-08 13:29:52 +0200

    CatProgram initialized

---
+++ CatProgram/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CatProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tämä on kissaohjelma!");
+
+            Cat myCat = new Cat();
+            Cat cat = new Cat(3);
+
+            Console.WriteLine("Press any key to continue!");
+            Console.ReadKey();
+        }
+    }
+}
