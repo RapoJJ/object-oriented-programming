db2bbca28bd2c40e1797c7707f864693b9b5d15c
udwee <jonttu.92@hotmail.com>
2019-01-08 15:18:17 +0200

    BookProgram initialized

---
+++ BookProgram/BookProgram/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BookProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
