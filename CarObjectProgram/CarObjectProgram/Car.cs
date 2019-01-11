a9cd965dbdf4bc785d330a254ae4311e35fecd24
udwee <jonttu.92@hotmail.com>
2019-01-11 14:40:19 +0200

    CarObjectProgram added UserInterface and brake+accelerate methods added

--- CarObjectProgram/CarObjectProgram/Car.cs
+++ CarObjectProgram/CarObjectProgram/Car.cs
@@ -9,21 +9,52 @@ namespace CarObjectProgram
         public string brand;
         public int speed;
 
+        public Car (string brand, int speed)
+        {
+            this.brand = brand;
+            this.speed = speed;
+        }
         public void AskData()
         {
-            Console.Write("Syötä auton merkki: ");
+            Console.Write("Brand name: ");
             this.brand = Console.ReadLine();
-            Console.Write("Syötä auton nopeus: ");
-            this.speed = int.Parse(Console.ReadLine());
+            Console.Write("Speed: ");
+            string speedValue = Console.ReadLine();
+            bool isNumberC = int.TryParse(speedValue, out int speed);
+            if (isNumberC)
+            {
+                this.speed = speed;
+            }
+            else
+            {
+                Console.WriteLine("Input wasnt number or it was empty, speed of car is 0.");
+                this.speed = 0;
+            }
         }
         public void ShowCarInfo()
         {
-            Console.WriteLine($"Auton {this.brand} nopeus on {this.speed}.");
+            Console.WriteLine($"Brand {this.brand} has speed of {this.speed}.");
         }
-
         public void Accelerate(int speedIncrease)
         {
-            this.speed += speedIncrease;
+            if (speedIncrease > 0)
+            {
+                this.speed += speedIncrease;
+                Console.WriteLine($"Car {this.brand} used ACCELERATE! New speed is {this.speed}.");
+            }
+            else if (speedIncrease == 0)
+            {
+                Console.WriteLine($"Car {this.brand} didn't use ACCELERATE! Speed is still {this.speed}.");
+            }
+            else
+            {
+                Console.WriteLine("Negative accelerating isn't possible!");
+            }
+        }
+        public void Brake()
+        {
+            this.speed = this.speed * 9 / 10;
+            Console.WriteLine($"Car {this.brand} used BRAKE! The new speed is {this.speed}.");
         }
     }
 }
--- CarObjectProgram/CarObjectProgram/Program.cs
+++ CarObjectProgram/CarObjectProgram/Program.cs
@@ -6,14 +6,67 @@ namespace CarObjectProgram
     {
         static void Main(string[] args)
         {
-            Car firstCar = new Car();
-            firstCar.AskData();
-            firstCar.ShowCarInfo();
+            ConsoleKeyInfo cki;
+            Car onlyCar = new Car("brand", 0);
+            do
+            {
+                cki = UserInterface();
+                Console.WriteLine();
+                switch (cki.Key)
+                {
+                    case ConsoleKey.F1:
+                        onlyCar.AskData();
+                        break;
+                    case ConsoleKey.F2:
+                        onlyCar.ShowCarInfo();
+                        break;
+                    case ConsoleKey.F3:
+                        onlyCar.Accelerate(UserAccelerate());
+                        break;
+                    case ConsoleKey.F4:
+                        onlyCar.Brake();
+                        break;
+                    case ConsoleKey.Escape:
+                        Console.WriteLine("Program closes now!");
+                        break;
+                    default:
+                        Console.WriteLine("The unexpected happened!");
+                        break;
+                }
+                Console.WriteLine("Press anykey!");
+                Console.ReadKey();
+                Console.Clear();   
+            } while (cki.Key != ConsoleKey.Escape);
 
-            Car secondCar = new Car();
-            secondCar.AskData();
-            secondCar.ShowCarInfo();
+        }
+        static ConsoleKeyInfo UserInterface()
+        {
+            Console.WriteLine("Choose what you want to do.");
+            Console.WriteLine("[F1] Input car info.");
+            Console.WriteLine("[F2] Print cars info.");
+            Console.WriteLine("[F3] Accelerate car.");
+            Console.WriteLine("[F4] Brake car.");
+            Console.WriteLine("[Esc] Close program");
 
+            return Console.ReadKey();
+        }
+        static int UserAccelerate()
+        {
+            bool isNumber;
+            while (true)
+            {
+                Console.Write("Accelerate amount: ");
+                isNumber = int.TryParse(Console.ReadLine(), out int accelerate);
+
+                if (isNumber)
+                {
+                    return accelerate;
+                }
+                else
+                {
+                    Console.WriteLine("Input has to be number! Try again!");
+                }
+            }
         }
     }
 }
