0a2bd2e5a9f97bfd675e77e8f674f970d5fa53d6
udwee <jonttu.92@hotmail.com>
2019-01-11 15:34:43 +0200

    CompanyClass version 1 done

---
+++ CompanyClass/CompanyClass/Company.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompanyClass
+{
+    class Company
+    {
+        public string title;
+        public string address;
+        public string phoneNr;
+        public int income;
+        public int expense;
+
+
+        public Company()
+        {
+            this.title = "title";
+            this.address = "address";
+            this.phoneNr = "phoneNr";
+            this.income = 0;
+            this.expense = 0;
+        }
+
+        public Company(string title, string address, string phoneNr, int income, int expense)
+        {
+            this.title = title;
+            this.address = address;
+            this.phoneNr = phoneNr;
+            this.income = income;
+            this.expense = expense;
+        }
+
+        public Company(Company previousCompany)
+        {
+            this.title = previousCompany.title;
+            this.address = previousCompany.address;
+            this.phoneNr = previousCompany.phoneNr;
+            this.income = previousCompany.income;
+            this.expense = previousCompany.expense;
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Company title: {this.title}");
+            Console.WriteLine($"Company address: {this.address}");
+            Console.WriteLine($"Company phoneNr: {this.phoneNr}");
+            Console.WriteLine($"Company income: {this.income}");
+            Console.WriteLine($"Company expense: {this.expense}");
+            Console.WriteLine("--------------------------------------------------");
+        }
+        public void Profit()
+        {
+            double profit = 100 * (this.income - this.expense) / this.expense;
+            Console.WriteLine($"Companys profit is {profit} %.");
+        }
+    }
+}
---
+++ CompanyClass/CompanyClass/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CompanyClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Company maestro = new Company("maestro", "lappee", "040", 500000, 450000);
+            Company visma = new Company(maestro);
+
+            visma.PrintInfo();
+            maestro.PrintInfo();
+
+            maestro.Profit();
+            visma.Profit();
+        }
+    }
+}
