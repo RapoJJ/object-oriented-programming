679576cc6bc364b66e65d9cf04599b1c75471fe9
udwee <jonttu.92@hotmail.com>
2019-01-17 11:44:47 +0200

    Made array of employees and salary comparison is done

--- EmployeeClass/EmployeeClass/Employee.cs
+++ EmployeeClass/EmployeeClass/Employee.cs
@@ -11,20 +11,21 @@ namespace EmployeeClass
         
         public int id;
         public int salary;
+        private static int counter = 0;
 
         public Employee()
         {
-            this.name = "name";
-            this.position = "position";
+            this.name = System.String.Empty;
+            this.position = System.String.Empty;
             this.salary = 0;
-            this.id = 0;
+            this.id = counter++;
         }
-        public Employee(string name, string position, int salary, int id)
+        public Employee(string name, string position, int salary)
         {
             this.name = name;
             this.position = position;
             this.salary = salary;
-            this.id = id;
+            this.id = counter++;
         }
         public void PrintEmployeeInfo()
         {
@@ -36,21 +37,21 @@ namespace EmployeeClass
         }
         public void CompareSalary(Employee employee)
         {
-            Console.WriteLine($"{this.name} salary is {this.salary}. {employee.name} salary is {employee.salary}.");
-            
+            Console.WriteLine($"{this.name}s salary is {this.salary}. {employee.name}s salary is {employee.salary}.");
+
             if (this.salary > employee.salary)
             {
-                Console.WriteLine($"{this.name} gets paid more than {employee.name} by {this.salary - employee.salary}");
+                Console.WriteLine($"{this.name} gets paid more than {employee.name} by {this.salary - employee.salary}.");
             }
             else if (this.salary == employee.salary)
             {
-                Console.WriteLine($"{this.name} and {employee.name} have the same pay of {this.salary}");
+                Console.WriteLine($"{this.name} and {employee.name} have the same pay of {this.salary}.");
             }
             else
             {
-                Console.WriteLine($"{employee.name} gets paid more than {this.name} by {employee.salary - this.salary}");
+                Console.WriteLine($"{employee.name} gets paid more than {this.name} by {employee.salary - this.salary}.");
             }
-
+            Console.WriteLine("---------------------------------");
         }
     }
 }
--- EmployeeClass/EmployeeClass/Program.cs
+++ EmployeeClass/EmployeeClass/Program.cs
@@ -6,6 +6,33 @@ namespace EmployeeClass
     {
         static void Main(string[] args)
         {
+
+            /*Employee first = new Employee("Antti", "TJ", 4500, 1);
+            Employee second = new Employee("Matti", "Tyäukko", 1800, 2);
+
+            first.PrintEmployeeInfo();
+            second.PrintEmployeeInfo();
+
+            first.CompareSalary(second);*/
+
+            Employee[] employees = new Employee[3];
+
+            employees[0] = new Employee("Matti", "Team leader", 5000);
+            employees[1] = new Employee("Tapio", "Programmer", 1600);
+            employees[2] = new Employee("Jaakko", "Cleaner", 900);
+
+
+            for (int i = 0; i < employees.Length; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+            
+            //employees[1].PrintEmployeeInfo();
+            //employees[2].PrintEmployeeInfo();
+
+            employees[0].CompareSalary(employees[1]);
+            employees[0].CompareSalary(employees[2]);
+            employees[1].CompareSalary(employees[2]);
         }
     }
 }
