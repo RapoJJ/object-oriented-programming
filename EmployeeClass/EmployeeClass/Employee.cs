4bd057240353746095bbe8cb3fe34388d3605e11
udwee <jonttu.92@hotmail.com>
2019-01-17 13:08:19 +0200

    Added List for employees and sorted them by salary

--- EmployeeClass/EmployeeClass/Employee.cs
+++ EmployeeClass/EmployeeClass/Employee.cs
@@ -53,5 +53,9 @@ namespace EmployeeClass
             }
             Console.WriteLine("---------------------------------");
         }
+        public override string ToString()
+        {
+            return $"Employee: {this.name}\nId: {this.id}\nPosition: {this.position}\nSalary: {this.salary}\n------------------";
+        }
     }
 }
--- EmployeeClass/EmployeeClass/Program.cs
+++ EmployeeClass/EmployeeClass/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace EmployeeClass
 {
@@ -6,7 +8,6 @@ namespace EmployeeClass
     {
         static void Main(string[] args)
         {
-
             /*Employee first = new Employee("Antti", "TJ", 4500, 1);
             Employee second = new Employee("Matti", "Tyäukko", 1800, 2);
 
@@ -15,7 +16,7 @@ namespace EmployeeClass
 
             first.CompareSalary(second);*/
 
-            Employee[] employees = new Employee[3];
+            /*Employee[] employees = new Employee[3];
 
             employees[0] = new Employee("Matti", "Team leader", 5000);
             employees[1] = new Employee("Tapio", "Programmer", 1600);
@@ -33,6 +34,21 @@ namespace EmployeeClass
             employees[0].CompareSalary(employees[1]);
             employees[0].CompareSalary(employees[2]);
             employees[1].CompareSalary(employees[2]);
+             */
+            List<Employee> employeeList = new List<Employee>()
+            {
+                new Employee{name = "Matti", position ="Team leader", salary = 5000},
+                new Employee{name = "Tapio", position ="Programmer", salary = 5},
+                new Employee{name = "Jonne", position ="Cleaner", salary = 1900}
+            };
+
+            employeeList.ForEach(Console.WriteLine);
+
+
+            Console.WriteLine("");
+            employeeList.Sort((x, y) => x.salary - y.salary);
+
+            employeeList.ForEach(Console.WriteLine);
         }
     }
 }
