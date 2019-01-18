447d3a9a57a790181d7b0ddae710796bc89e17db
udwee <jonttu.92@hotmail.com>
2019-01-18 11:05:41 +0200

    Added new class for Ticket, Customer.cs needs to be edited!

--- TicketPrice/TicketPrice/Customer.cs
+++ TicketPrice/TicketPrice/Customer.cs
@@ -6,9 +6,63 @@ namespace TicketPrice
 {
     class Customer
     {
+        public double ticketPrice = 16;
         public int age;
-        public bool mtkMember;
-        public bool student;
-        public bool conscript;
+        public bool isMtkMember;
+        public bool isStudent;
+        public bool isConscript;
+
+        public Customer()
+        {
+            this.age = 0;
+            this.isMtkMember = true;
+            this.isStudent = true;
+            this.isConscript = true;
+        }
+        public Customer(int age, bool member, bool student, bool conscript)
+        {
+            this.age = age;
+            this.isMtkMember = member;
+            this.isStudent = student;
+            this.isConscript = conscript;
+        }
+
+        public void TicketCalculator()
+        {
+            double discount = 0;
+
+            if (this.age < 7)
+            {
+                discount = 1;
+            }
+            else if (this.age > 65)
+            {
+                discount = 0.5;
+            }
+            else if (this.age >= 7 && this.age <= 15)
+            {
+                discount = 0.5;
+            }
+            else
+            {
+                if (this.isConscript)
+                {
+                    discount = 0.45;
+                }
+                else if (this.isMtkMember && this.isStudent)
+                {
+                    discount = 0.60;
+                }
+                else if (this.isStudent)
+                {
+                    discount = 0.45;
+                }
+                else if (this.isMtkMember)
+                {
+                    discount = 0.15;
+                }
+            }
+            this.ticketPrice = ticketPrice - ticketPrice * discount;
+        }
     }
 }
--- TicketPrice/TicketPrice/Program.cs
+++ TicketPrice/TicketPrice/Program.cs
@@ -6,7 +6,50 @@ namespace TicketPrice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int age = UserAgeInput();
+            
+        }
+   
+        static int UserAgeInput()
+        {
+            while (true)
+            {
+                Console.Write("Input age: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int age);
+                if (isNumber)
+                {
+                    return age;
+                }
+                else
+                {
+                    Console.WriteLine("Input wasnt number! Try again.");
+                }
+            }
+        }
+        static bool UserInfoInput(string question)
+        {
+            while (true)
+            {
+                Console.WriteLine(question + " ([1] = YES [2] = NO)");
+                Console.Write("Input: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int answer);
+
+                if (isNumber && answer == 1 || answer == 2)
+                {
+                    if (answer == 1)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input wasnt [1] or [2]");
+                }
+            }
         }
     }
 }
