bc3cf41b661e500fa2cfbb9ba4866d968a38753d
udwee <jonttu.92@hotmail.com>
2019-01-18 09:37:11 +0200

    TicketPrice initialized and Customer class created

---
+++ TicketPrice/TicketPrice/Customer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicketPrice
+{
+    class Customer
+    {
+        public int age;
+        public bool mtkMember;
+        public bool student;
+        public bool conscript;
+    }
+}
