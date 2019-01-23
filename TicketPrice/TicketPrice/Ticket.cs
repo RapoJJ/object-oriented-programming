4baafe30d06a6311a337b966cf331a58cab4e1cf
udwee <jonttu.92@hotmail.com>
2019-01-23 10:35:44 +0200

    Added prints to age discounts

--- TicketPrice/TicketPrice/Ticket.cs
+++ TicketPrice/TicketPrice/Ticket.cs
@@ -19,16 +19,19 @@ namespace TicketPrice
         {
             if (customer.age < 7 )
             {
+                Console.WriteLine("Customer is under 7 years old, ticket discount is 100 %.");
                 isTicketPrice = true;
                 return price - price * 1;
             }
             else if (customer.age > 65)
             {
+                Console.WriteLine("Customer is over 65 years old, ticket discount is 50 %.");
                 isTicketPrice = true;
                 return price - price * 0.5;
             }
             else if (customer.age >= 7 && customer.age <= 15)
             {
+                Console.WriteLine("Customer is between 7 and 15 years old, ticket discount is 50 %.");
                 isTicketPrice = true;
                 return price - price * 0.5;
             }
