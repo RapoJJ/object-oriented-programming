8980c17887bcff919a799825cace0c6dadee37a7
udwee <jonttu.92@hotmail.com>
2019-01-24 08:31:31 +0200

    Renamed price to _price in ticket.cs

--- TicketPrice/TicketPrice/Ticket.cs
+++ TicketPrice/TicketPrice/Ticket.cs
@@ -6,12 +6,12 @@ namespace TicketPrice
 {
     class Ticket
     {
-        private double price;
+        private double _price;
         public bool isTicketPrice;
 
         public Ticket()
         {
-            this.price = 16.00;
+            this._price = 16.00;
             this.isTicketPrice = false;
         }
 
@@ -21,40 +21,40 @@ namespace TicketPrice
             {
                 Console.WriteLine("Customer is under 7 years old, ticket discount is 100 %.");
                 isTicketPrice = true;
-                return price - price * 1;
+                return _price - _price * 1;
             }
             else if (customer.age > 65)
             {
                 Console.WriteLine("Customer is over 65 years old, ticket discount is 50 %.");
                 isTicketPrice = true;
-                return price - price * 0.5;
+                return _price - _price * 0.5;
             }
             else if (customer.age >= 7 && customer.age <= 15)
             {
                 Console.WriteLine("Customer is between 7 and 15 years old, ticket discount is 50 %.");
                 isTicketPrice = true;
-                return price - price * 0.5;
+                return _price - _price * 0.5;
             }
             else
             {
                 if (customer.isConscript)
                 {
-                    return price - price * 0.50;
+                    return _price - _price * 0.50;
                 }
                 else if (customer.isMtkMember && customer.isStudent)
                 {
-                    return price - price * 0.60;
+                    return _price - _price * 0.60;
                 }
                 else if (customer.isStudent)
                 {
-                    return price - price * 0.45;
+                    return _price - _price * 0.45;
                 }
                 else if (customer.isMtkMember)
                 {
-                    return price - price * 0.15;
+                    return _price - _price * 0.15;
                 }
                 else
-                    return price;
+                    return _price;
             }
         }
     }
