699290c77ab5f2af3ffac1e2741482157923cd94
udwee <jonttu.92@hotmail.com>
2019-01-29 09:51:40 +0200

    Vehicle contructors and virtual PrintInfo done, Car Class initialized and default contructor done

---
+++ TransportVehicle/TransportVehicle/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TransportVehicle
+{
+    class Car : Vehicle
+    {
+        protected double engine;
+        protected string model;
+        protected int doorAmount;
+
+        public Car()
+        {
+            engine = 2.0;
+            model = string.Empty;
+            doorAmount = 4;
+        }
+    }
+}
--- TransportVehicle/TransportVehicle/Vehicle.cs
+++ TransportVehicle/TransportVehicle/Vehicle.cs
@@ -6,5 +6,31 @@ namespace TransportVehicle
 {
     class Vehicle
     {
+        protected string type;
+        protected string brand;
+        protected int yearModel;
+        protected int price;
+
+        public Vehicle()
+        {
+            type = string.Empty;
+            brand = string.Empty;
+            yearModel = 1900;
+            price = 0;
+        }
+        public Vehicle(string type, string brand, int year, int price)
+        {
+            this.type = type;
+            this.brand = brand;
+            this.yearModel = year;
+            this.price = price;
+        }
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine($"Vehicle type: {this.type}" +
+                $"Brand: {this.brand}" +
+                $"Model year: {this.yearModel}" +
+                $"Price: {this.price}");
+        }
     }
 }
