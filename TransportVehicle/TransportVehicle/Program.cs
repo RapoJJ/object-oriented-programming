a4977a1e44211a232a5e45ab8bb2bc92824c8be8
udwee <jonttu.92@hotmail.com>
2019-01-29 08:44:07 +0200

    TransportVehicle initialized with new Class Vehicle

---
+++ TransportVehicle/TransportVehicle/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TransportVehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
