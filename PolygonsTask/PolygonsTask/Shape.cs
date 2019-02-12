e92a7d077df3779351e4b503b4e608d8cf214aee
udwee <jonttu.92@hotmail.com>
2019-02-12 09:14:50 +0200

    Shape abstract class build

---
+++ PolygonsTask/PolygonsTask/Shape.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolygonsTask
+{
+    abstract class Shape
+    {
+        private string _type;
+        private string _color;
+        abstract public double Area();
+        abstract public string GetInfo();
+
+        public string Type { get => _type; set => _type = value; }
+        public string Color { get => _color; set => _color = value; }
+
+        public Shape(string type, string color)
+        {
+            Type = type;
+            Color = color;
+        }
+
+        public string GetColor()
+        {
+            return this.Color;
+        }
+    }
+}
