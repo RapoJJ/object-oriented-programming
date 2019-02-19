b17188e183aec6d9dfe05b34b43d90546c5dcdc7
udwee <jonttu.92@hotmail.com>
2019-02-19 12:40:10 +0200

    Final version, changed color of triangle

---
+++ FileManager/FileManager/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager file = new FileManager(@"C:\TEMP\refNumbers.txt");
+
+            file.ReadWords();
+        }
+    }
+}
--- PolygonsTask/PolygonsTask/Program.cs
+++ PolygonsTask/PolygonsTask/Program.cs
@@ -10,13 +10,13 @@ namespace PolygonsTask
             List<Shape> shapes = new List<Shape>
             {
                 new Circle("Circle", "Red", 4),
-                new Triangle("Triangle", "Red",7,4),
+                new Triangle("Triangle", "Blue",7,4),
                 new Rectangle("Rectangle","Green",10,15)
             };
 
-//            foreach (var s in shapes)
-//                Console.WriteLine(s.ToString());
-
+            foreach (var s in shapes)
+                Console.WriteLine(s.ToString());
+            Console.WriteLine("-------------------");
             Console.WriteLine("Print only Circles");
             foreach (var type in shapes)
             {
