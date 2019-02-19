ef066807981fef16cae9ebf004814f101f10d0e2
udwee <jonttu.92@hotmail.com>
2019-02-19 12:58:58 +0200

    New FileManagerAndExceptions made

---
+++ FileManagerAndExceptions/FileManagerAndExceptions/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileManagerAndExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
